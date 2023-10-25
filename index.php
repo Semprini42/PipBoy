032f099c594925c0fbf126536106e524d43d5c85
YanUchitel <200519611@student.georgianc.on.ca>
2023-10-25 11:02:56 -0400

    got map to work, use own api key

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" prefix="og: http://ogp.me/ns#">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
@@ -17,8 +18,42 @@
     <meta property='og:image' content='/reference2.jpg' />
     <link rel="icon" href="/images/gear.png" type="image/x-icon" />
 
-    <link rel="stylesheet" type="text/css" href="css/main.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/main.min.css" />
+
+    <script>
+        (g => {
+            var h, a, k, p = "The Google Maps JavaScript API",
+                c = "google",
+                l = "importLibrary",
+                q = "__ib__",
+                m = document,
+                b = window;
+            b = b[c] || (b[c] = {});
+            var d = b.maps || (b.maps = {}),
+                r = new Set,
+                e = new URLSearchParams,
+                u = () => h || (h = new Promise(async (f, n) => {
+                    await (a = m.createElement("script"));
+                    e.set("libraries", [...r] + "");
+                    for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+                    e.set("callback", c + ".maps." + q);
+                    a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                    d[q] = f;
+                    a.onerror = () => h = n(Error(p + " could not load."));
+                    a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                    m.head.append(a)
+                }));
+            d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+        })({
+            key: "<API_KEY>",
+            v: "weekly",
+            // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+            // Add other bootstrap parameters as needed, using camel case.
+        });
+    </script>
+    </script>
 </head>
+
 <body>
     <div class="note">
         Please note: this app is designed for bigger screens!<br>
@@ -132,7 +167,7 @@
 
             <div class="screen" data-screen="4">
                 <div id="map">
-                    Please check your internet connection to use map!
+                    
                 </div>
             </div>
 
@@ -176,9 +211,10 @@
     </div>
 
     <footer>
-        Copyright &copy; <?=date('Y');?>. FEDIRKO.PRO and Bethesda Game Studios
+        Copyright &copy; <?= date('Y'); ?>. FEDIRKO.PRO and Bethesda Game Studios
         <a target="_blank" class="footer_author" href="//fedirko.pro" title="created by FEDIRKO.PRO"></a>
     </footer>
-<script src="js/main.min.js"></script>
+    <script src="js/main.min.js"></script>
 </body>
+
 </html>
